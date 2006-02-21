a7e96681116f60ef3bb57bd521a1ea5a6af5a9d8
Tommi Virtanen <tv@inoi.fi>
2006-02-22 00:44:35 +0200

    Add small blurb about webut.skin

---
+++ webut/skin/__init__.py
@@ -0,0 +1,7 @@
+"""
+Skinnable web apps.
+
+The most important requirement for this all is that the content pages
+must not inherit the skin. This ensures the content need not know
+about the used skin.
+"""
